70c8025b6746d221910b315881d7511d76016b9b
Michael James <mike.pisula@gmail.com>
2018-12-09 18:35:19 -0600

    Initial Commit

---
+++ httprequest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  httprequest
+//
+//  Created by Michael James on 12/9/18.
+//  Copyright © 2018 Michael James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
